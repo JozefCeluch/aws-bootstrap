567e5936ce844878428aca028859b414b9cd49a0
Jozef Celuch <jozef@novoda.com>
2020-04-13 10:15:35 +0200

    Add second instance

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -50,6 +50,6 @@ aws cloudformation deploy \
 if [ $? -eq 0 ]; then
     aws cloudformation list-exports \
         --profile awsbootstrap \
-        --query "Exports[?Name=='InstanceEndpoint'].Value"
+        --query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
 fi
     
