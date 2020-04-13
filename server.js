081112ffb680fd050613899d2cf255456e59551b
Jozef Celuch <jozef@novoda.com>
2020-04-13 18:26:40 +0200

    Fix errors in JS code

--- server.js
+++ server.js
@@ -9,20 +9,17 @@ const httpsPort = 8443;
 const httpsKey = '../keys/key.pem';
 const httpsCert = '../keys/cert.pem';
 
-if (fs.existsSync(httpsKey) && fs.exists(httpsCert)) {
-  console.log('Starting https server');
-  const message = `Hello HTTPS world from ${hostname()} in ${STACK_NAME}\n`;
-  const options = {
-    key: fs.readFileSync(httpsKey),
-    cert: fs.readFileSync(httpsCert)
-  }
+if (fs.existsSync(httpsKey) && fs.existsSync(httpsCert)) {
+  console.log('Starting https server')
+  const message = `Hello HTTPS World from ${hostname()} in ${STACK_NAME}\n`;
+  const options = { key: fs.readFileSync(httpsKey), cert: fs.readFileSync(httpsCert) };
   const server = https.createServer(options, (req, res) => {
-    res.statusCode = 200;
-    res.setHeader = ('Content-Type', 'text/plain');
-    res.end(message);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(message);
   });
   server.listen(httpsPort, hostname, () => {
-    console.log(`Server running at http://${hostname}:${httpsPort}`);
+      console.log(`Server running at http://${hostname()}:${httpsPort}/`);
   });
 }
 
